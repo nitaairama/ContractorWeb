538f6b50b5140365905593b45a6953835a880fe8
unknown <nitaairama@gmail.com>
2026-04-01 11:38:49 +0800

    Edit models

--- app/Models/Portofolio.php
+++ app/Models/Portofolio.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Portofolio extends Model
 {
-    //
+    // Mengizinkan field untuk diisi
+    protected $fillabel = [
+        'title',
+        'description',
+        'image',
+    ];
 }
