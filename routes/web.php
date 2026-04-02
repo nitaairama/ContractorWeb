4d02caa3734f4939c61ab035c3df03164ac01122
unknown <nitaairama@gmail.com>
2026-04-02 11:27:57 +0800

    Edit routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortofolioController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route publik
+Route::get('/', [PortofolioController::class, 'index']);
+
+// Route admin
+Route::prefix('admin')->group(function () {
+    // Tampilkan semua data
+    Route::get('/portofolio', [PortofolioController::class, 'adminIndex']);
+
+    // Form tambah data
+    Route::get('/portofolio/create', [PortofolioController::class, 'create']);
+
+    // Simpan data
+    Route::post('portofolio/store', [PortofolioController::class. 'store']);
+
+    // Form edit data
+    Route::get('/portofolio/edit{id}', [PortofolioController::class, 'edit']);
+
+    // Update data
+    Route::post('/portofolio/update{id}', [PortofolioController::class, 'update']);
+
+    // Menghapus data
+    Route::delete('/portofolio/delete{id}', [PortofolioController::class, 'delete']);
 });
